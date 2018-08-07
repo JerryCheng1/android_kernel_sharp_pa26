a2b3e2495df221f22b414b40a9af6f3d9a70db11
JerryCheng <501950452@qq.com>
2018-08-07 22:41:10 +0800

    pa26: panic into recovery

--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -255,8 +255,14 @@ static void msm_restart_prepare(const char *cmd)
 	/* This looks like a normal reboot at this point. */
 	set_dload_mode(0);
 
-	/* Write download mode flags if we're panic'ing */
-	set_dload_mode(in_panic);
+
+	/* ## We don't want to panic into dload mode ##
+	 * change in set_dload_mode(in_panic) to re-enable it
+	 * But I want to panic into TWRP and cat last_kmsg
+	 */
+	//set_dload_mode(in_panic);
+	if (in_panic == 1)
+		cmd = "recovery";
 
 	/* Write download mode flags if restart_mode says so */
 	if (restart_mode == RESTART_DLOAD)
